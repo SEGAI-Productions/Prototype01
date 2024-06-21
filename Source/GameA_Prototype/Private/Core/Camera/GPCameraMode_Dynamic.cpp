05dbec579dd0a44ab6d46a8bcc6721086327dd06
mitchell9423 <37316395+mitchell9423@users.noreply.github.com>
2024-06-21 17:00:46 -0400

    Initial Camera Framework

---
+++ Source/GameA_Prototype/Private/Core/Camera/GPCameraMode_Dynamic.cpp
@@ -0,0 +1,123 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/Camera/GPCameraMode_Dynamic.h"
+#include "Core/Camera/GPCameraMode.h"
+#include "Components/PrimitiveComponent.h"
+#include "Curves/CurveVector.h"
+#include "Engine/Canvas.h"
+#include "GameFramework/CameraBlockingVolume.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/Character.h"
+#include "Math/RotationMatrix.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(GPCameraMode_Dynamic)
+
+UGPCameraMode_Dynamic::UGPCameraMode_Dynamic()
+{
+	TargetOffsetCurve = nullptr;
+}
+
+void UGPCameraMode_Dynamic::UpdateView(float DeltaTime)
+{
+	UpdateForTarget(DeltaTime);
+	UpdateCrouchOffset(DeltaTime);
+
+	FVector PivotLocation = GetPivotLocation() + CurrentCrouchOffset;
+	FRotator PivotRotation = GetPivotRotation();
+
+	PivotRotation.Pitch = FMath::ClampAngle(PivotRotation.Pitch, ViewPitchMin, ViewPitchMax);
+
+	View.Location = PivotLocation;
+	View.Rotation = PivotRotation;
+	View.ControlRotation = View.Rotation;
+	View.FieldOfView = FieldOfView;
+
+	// Apply Dynamic offset using pitch.
+	if (!bUseRuntimeFloatCurves)
+	{
+		if (TargetOffsetCurve)
+		{
+			const FVector TargetOffset = TargetOffsetCurve->GetVectorValue(PivotRotation.Pitch);
+			View.Location = PivotLocation + PivotRotation.RotateVector(TargetOffset);
+		}
+	}
+	else
+	{
+		FVector TargetOffset(0.0f);
+
+		TargetOffset.X = TargetOffsetX.GetRichCurveConst()->Eval(PivotRotation.Pitch);
+		TargetOffset.Y = TargetOffsetY.GetRichCurveConst()->Eval(PivotRotation.Pitch);
+		TargetOffset.Z = TargetOffsetZ.GetRichCurveConst()->Eval(PivotRotation.Pitch);
+
+		View.Location = PivotLocation + PivotRotation.RotateVector(TargetOffset);
+	}
+
+	// Adjust final desired camera location to prevent any penetration
+	UpdatePreventPenetration(DeltaTime);
+}
+
+void UGPCameraMode_Dynamic::UpdateForTarget(float DeltaTime)
+{
+	if (const ACharacter* TargetCharacter = Cast<ACharacter>(GetTargetActor()))
+	{
+		if (TargetCharacter->bIsCrouched)
+		{
+			const ACharacter* TargetCharacterCDO = TargetCharacter->GetClass()->GetDefaultObject<ACharacter>();
+			const float CrouchedHeightAdjustment = TargetCharacterCDO->CrouchedEyeHeight - TargetCharacterCDO->BaseEyeHeight;
+
+			SetTargetCrouchOffset(FVector(0.f, 0.f, CrouchedHeightAdjustment));
+
+			return;
+		}
+	}
+
+	SetTargetCrouchOffset(FVector::ZeroVector);
+}
+
+void UGPCameraMode_Dynamic::UpdatePreventPenetration(float DeltaTime)
+{
+}
+
+void UGPCameraMode_Dynamic::PreventCameraPenetration(AActor const& ViewTarget, FVector const& SafeLoc, FVector& CameraLoc, float const& DeltaTime, float& DistBlockedPct, bool bSingleRayOnly)
+{
+}
+
+void UGPCameraMode_Dynamic::DrawDebug(UCanvas* Canvas) const
+{
+	Super::DrawDebug(Canvas);
+
+#if ENABLE_DRAW_DEBUG
+	FDisplayDebugManager& DisplayDebugManager = Canvas->DisplayDebugManager;
+	for (int i = 0; i < DebugActorsHitDuringCameraPenetration.Num(); i++)
+	{
+		DisplayDebugManager.DrawString(
+			FString::Printf(TEXT("HitActorDuringPenetration[%d]: %s")
+				, i
+				, *DebugActorsHitDuringCameraPenetration[i]->GetName()));
+	}
+
+	LastDrawDebugTime = GetWorld()->GetTimeSeconds();
+#endif
+}
+
+void UGPCameraMode_Dynamic::SetTargetCrouchOffset(FVector NewTargetOffset)
+{
+	CrouchOffsetBlendPct = 0.0f;
+	InitialCrouchOffset = CurrentCrouchOffset;
+	TargetCrouchOffset = NewTargetOffset;
+}
+
+void UGPCameraMode_Dynamic::UpdateCrouchOffset(float DeltaTime)
+{
+	if (CrouchOffsetBlendPct < 1.0f)
+	{
+		CrouchOffsetBlendPct = FMath::Min(CrouchOffsetBlendPct + DeltaTime * CrouchOffsetBlendMultiplier, 1.0f);
+		CurrentCrouchOffset = FMath::InterpEaseInOut(InitialCrouchOffset, TargetCrouchOffset, CrouchOffsetBlendPct, 1.0f);
+	}
+	else
+	{
+		CurrentCrouchOffset = TargetCrouchOffset;
+		CrouchOffsetBlendPct = 1.0f;
+	}
+}
